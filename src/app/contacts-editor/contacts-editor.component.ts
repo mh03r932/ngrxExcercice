845aa5191df457f9b562437ec7da18bb49874a93
dvbern <markus.hofstetter@dvbern.ch>
2018-04-28 18:15:01 +0200

    step6 effect hydrating > replace direct service call while saving with effect

--- src/app/contacts-editor/contacts-editor.component.ts
+++ src/app/contacts-editor/contacts-editor.component.ts
@@ -46,11 +46,12 @@ export class ContactsEditorComponent implements OnInit {
   }
 
   save(contact: Contact) {
-    this.contactsService.updateContact(contact)
-      .subscribe(() => {
-        this.store.dispatch(new UpdateContactAction(contact));
-        this.goToDetails(contact);
-      });
+    this.store.dispatch(new UpdateContactAction(contact));
+    // this.contactsService.updateContact(contact)
+    //   .subscribe(() => {
+    //     this.store.dispatch(new UpdateContactAction(contact));
+    //     this.goToDetails(contact);
+    //   });
 
 
   }
